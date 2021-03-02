8eea5d5fb599410ba5e22002f0f5db4f2fdba69d
HugoSanchez <hugosanchez251@gmail.com>
2021-03-02 12:59:42 +0100

    again with cors

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ let corsOptions = {
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({limit: '5000mb'}));
-app.use(express.urlencoded({limit: '5000mb', extended: true}));
+app.use(express.urlencoded({limit: '5000mb'}));
 
 
 const CollectionsController = require('./src/collections/CollectionsController');
