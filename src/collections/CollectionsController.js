5b06b2d7b1c677d21be27a6a281c3ad7ac340382
HugoSanchez <hugosanchez251@gmail.com>
2021-04-16 15:25:49 +0200

    deleted old endpoints for collections

--- src/collections/CollectionsController.js
+++ src/collections/CollectionsController.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const client = require('../../client')
 const fleekStorage = require('@fleekhq/fleek-storage-js');
-const {ThreadID,} = require('@textile/hub');
+const {ThreadID} = require('@textile/hub');
 const multer  = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const {encrypt, decrypt} = require('../utils');
+const User = require('../user/User');
 const cors = require('cors')
 
 
@@ -50,15 +52,25 @@ router.post('/uploadToIpfs', cors(), upload.any(), async function (req, res) {
 // Ethereum address. Returns collections array.
 // This route is called when user is not logged in yet
 // so that there could be public profiles. This is temporary.
-router.get('/collections/:owner', cors(), async function (req, res) {
+router.get('/collections/:address', cors(), async function (req, res) {
+    
+    console.time('e')
+    let TexClient = await client()
+    console.timeEnd('e')
+  
+    let address = encrypt(req.params.address)
+    let user = await User.findOne({address}).exec()
+    let masterThreadID = decrypt(user.masterThreadID)
+    let previewEntriesThreadID = decrypt(user.previewEntriesThreadID)
 
-    let owner = req.params.owner   
-    let TexClient = await client()    
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)
-    let rawCollections = await TexClient.find(threadID, 'public-collections', {})
-    let collections = rawCollections.filter(collection => collection.owner.ethAddress === owner)
+    
+    let threadID = await ThreadID.fromString(masterThreadID)
+    let collections = await TexClient.find(threadID, 'collections-list', {})
 
-    res.status(200).send({success: true, collections})
+    let threadIDii = await ThreadID.fromString(previewEntriesThreadID)
+    let previewEntries = await TexClient.find(threadIDii, 'preview-entries', {})
+
+    res.status(200).send({success: true, collections, previewEntries})
 });
 
 
@@ -66,91 +78,27 @@ router.get('/collections/:owner', cors(), async function (req, res) {
 // It, gets all collections, filters to a specific collection,
 // and retrrieves all entries from that collection.
 // This is very ineficient and should be changed.
-router.get('/collections/:owner/:collectionName', async function (req, res) {
+router.get('/collections/:owner/:threadID', async function (req, res) {
 
-    let owner = req.params.owner
-    let colName = req.params.collectionName 
+    let collectionID = req.params.threadID 
 
     let TexClient = await client() 
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)
-    let rawCollections = await TexClient.find(threadID, 'public-collections', {})
-    let collection = rawCollections.filter(collection => 
-        collection.owner.ethAddress === owner && collection.name === colName)
-
-    let colID = await ThreadID.fromString(collection[0].threadId)
-    let entries = await TexClient.find(colID, 'entries', {})
+    let threadID = await ThreadID.fromString(collectionID)
+    let entries = await TexClient.find(threadID, 'entries', {})
+    let collection = await TexClient.find(threadID, 'config', {})
 
     res.status(200).send({success: true, entries, collection})
 });
 
-router.get('/collections/:owner/:collectionName/:itemId', async function (req, res) {
-
-    let owner = req.params.owner
-    let itemId = req.params.itemId
-    let colName = req.params.collectionName 
+router.get('/collections/:owner/:threadID/:itemId', async function (req, res) {
 
     let TexClient = await client() 
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)
-    let rawCollections = await TexClient.find(threadID, 'public-collections', {})
-    let collection = rawCollections.filter(collection => 
-        collection.owner.ethAddress === owner && collection.name === colName)
-
-    let colID = await ThreadID.fromString(collection[0].threadId)
-    let entries = await TexClient.find(colID, 'entries', {})
-    let item = entries.filter(item => item._id === itemId)
+    let threadID = await ThreadID.fromString(req.params.threadID)
+    let entries = await TexClient.find(threadID, 'entries', {})
+    let collection = await TexClient.find(threadID, 'config', {})
+    let item = entries.filter(item => item._id === req.params.itemId)
 
     res.status(200).send({success: true, entries, collection, item})
-});
-
-router.post('/add/collections', async function (req, res) {
-
-    let collection = req.body
-
-    let TexClient = await client() 
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)    
-    await TexClient.create(threadID, 'public-collections', [collection])
-
-    res.status(200).send({success: true})
-});
-
-router.post('/delete/collections', async function (req, res) {
-
-    let collectionID = req.body.id
-
-    let TexClient = await client() 
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)    
-    
-    let rawCollections = await TexClient.find(threadID, 'public-collections', {})
-    let collection = rawCollections.filter(collection => 
-        collection.id === collectionID)
-    
-    if (collection[0]) {
-        let _ID = collection[0]._id
-        await TexClient.delete(threadID, 'public-collections', [_ID])
-    }
-
-    res.status(200).send({success: true})
-});
-
-router.post('/update/collections', async function (req, res) {
-
-    let collectionToUpdate = req.body.collection
-
-    console.log('COL: ', collectionToUpdate)
-
-    let TexClient = await client() 
-    let threadID = await ThreadID.fromString(process.env.GLOBAL_THREAD_ID)    
-    
-    let rawCollections = await TexClient.find(threadID, 'public-collections', {})
-    let collection = rawCollections.filter(collection => 
-        collection.id === collectionToUpdate.id)
-    
-    if (collection[0]) {
-        collectionToUpdate._id = collection[0]._id
-        await TexClient.save(threadID, 'public-collections', [collectionToUpdate])
-    }
-
-    res.status(200).send({success: true})
 });
 
 router.post('/follow', async function (req, res) {
